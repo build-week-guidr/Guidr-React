5aa75a30769284ed19e9cbed03920beecb196e17
Roger Lee <rmlee0003@gmail.com>
2019-07-23 21:41:14 -0400

    scaffolding

---
+++ src/components/Login.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Login extends Component {
+ constructor() {
+  super()
+  this.state = {
+   username:'',
+   password: '',
+  }
+ }
+ render() {
+  return (
+   <div>
+    
+   </div>
+  );
+ }
+}
+
+export default Login;
